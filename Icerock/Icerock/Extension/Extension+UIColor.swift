2fcbf17e9ef7f65defc4464bbc6270d74e120118
Maksim <maximklochov@gmail.com>
2022-05-13 18:32:06 +0300

    Добавлен экран авторизации

---
+++ Icerock/Icerock/Extension/Extension+UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+UIColor.swift
+//  Icerock
+//
+//  Created by Максим Клочков on 13.05.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let translucentWhite = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.5)
+    static let colorTwo = UIColor(red: 33/255, green: 38/255, blue: 45/255, alpha: 1)
+    static let colorThree = UIColor(red: 88/255, green: 166/255, blue: 255/255, alpha: 0.5)
+
+
+}
---
+++ Icerock/Icerock/LoginViewController.swift
@@ -0,0 +1,90 @@
+//
+//  LoginViewController.swift
+//  Icerock
+//
+//  Created by Максим Клочков on 13.05.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var tokenTextField: UITextField!
+    @IBOutlet weak var singInButton: UIButton!
+    private var isKeyboardAppeared = false
+    private var oldSingInButtonHeight: CGFloat?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+
+    @objc func backgroundTap(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+
+        if !isKeyboardAppeared {
+            oldSingInButtonHeight = self.view.frame.size.height
+            UIView.animate(withDuration: 0.2, animations: {
+                self.view.frame.size.height -= keyboardSize.height
+                self.view.layoutIfNeeded()
+            })
+            self.tokenTextField.layer.borderColor = UIColor.colorThree.cgColor
+            isKeyboardAppeared = true
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        guard let oldValue = oldSingInButtonHeight else { return }
+
+        if isKeyboardAppeared {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.view.frame.size.height = oldValue
+                self.view.layoutIfNeeded()
+            })
+            self.tokenTextField.layer.borderColor = UIColor.colorTwo.cgColor
+            isKeyboardAppeared = false
+        }
+    }
+
+    @IBAction func tapSingInButton(_ sender: Any) {
+        print(#function)
+    }
+
+    private func setupView() {
+        tokenTextField.layer.borderWidth = 1
+        tokenTextField.layer.borderColor = UIColor.colorTwo.cgColor
+        tokenTextField.layer.cornerRadius = 8
+        tokenTextField.textColor = .white
+        tokenTextField.attributedPlaceholder = NSAttributedString(
+            string: "Personal access token",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.translucentWhite]
+        )
+        tokenTextField.leftView = UIView(frame: CGRect(x: 0, y: 10, width: 10, height: tokenTextField.frame.height))
+        tokenTextField.leftViewMode = .always
+
+        setNeedsStatusBarAppearanceUpdate()
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+}
